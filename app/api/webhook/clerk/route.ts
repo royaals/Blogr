46c0436a8b73536ff104982e9d4e3254bec88aea
Royal <royal.21ai040@sode-edu.in>
2024-03-09 18:00:20 +0530

    initial commit

---
+++ app/api/webhook/clerk/route.ts
@@ -0,0 +1,193 @@
+
+import { Webhook, WebhookRequiredHeaders } from "svix";
+import { headers } from "next/headers";
+
+import { IncomingHttpHeaders } from "http";
+
+import { NextResponse } from "next/server";
+import {
+  addMemberToCommunity,
+  createCommunity,
+  deleteCommunity,
+  removeUserFromCommunity,
+  updateCommunityInfo,
+} from "@/lib/actions/community.actions";
+
+
+type EventType =
+  | "organization.created"
+  | "organizationInvitation.created"
+  | "organizationMembership.created"
+  | "organizationMembership.deleted"
+  | "organization.updated"
+  | "organization.deleted";
+
+type Event = {
+  data: Record<string, string | number | Record<string, string>[]>;
+  object: "event";
+  type: EventType;
+};
+
+export const POST = async (request: Request) => {
+  const payload = await request.json();
+  const header = headers();
+
+  const heads = {
+    "svix-id": header.get("svix-id"),
+    "svix-timestamp": header.get("svix-timestamp"),
+    "svix-signature": header.get("svix-signature"),
+  };
+
+
+  const wh = new Webhook(process.env.NEXT_CLERK_WEBHOOK_SECRET || "");
+
+  let evnt: Event | null = null;
+
+  try {
+    evnt = wh.verify(
+      JSON.stringify(payload),
+      heads as IncomingHttpHeaders & WebhookRequiredHeaders
+    ) as Event;
+  } catch (err) {
+    return NextResponse.json({ message: err }, { status: 400 });
+  }
+
+  const eventType: EventType = evnt?.type!;
+
+
+  if (eventType === "organization.created") {
+
+    const { id, name, slug, logo_url, image_url, created_by } =
+      evnt?.data ?? {};
+
+    try {
+
+      await createCommunity(
+
+        id,
+        name,
+        slug,
+        logo_url || image_url,
+        "org bio",
+        created_by
+      );
+
+      return NextResponse.json({ message: "User created" }, { status: 201 });
+    } catch (err) {
+      console.log(err);
+      return NextResponse.json(
+        { message: "Internal Server Error" },
+        { status: 500 }
+      );
+    }
+  }
+
+
+  if (eventType === "organizationInvitation.created") {
+    try {
+
+      console.log("Invitation created", evnt?.data);
+
+      return NextResponse.json(
+        { message: "Invitation created" },
+        { status: 201 }
+      );
+    } catch (err) {
+      console.log(err);
+
+      return NextResponse.json(
+        { message: "Internal Server Error" },
+        { status: 500 }
+      );
+    }
+  }
+
+  if (eventType === "organizationMembership.created") {
+    try {
+
+      const { organization, public_user_data } = evnt?.data;
+      console.log("created", evnt?.data);
+
+
+      await addMemberToCommunity(organization.id, public_user_data.user_id);
+
+      return NextResponse.json(
+        { message: "Invitation accepted" },
+        { status: 201 }
+      );
+    } catch (err) {
+      console.log(err);
+
+      return NextResponse.json(
+        { message: "Internal Server Error" },
+        { status: 500 }
+      );
+    }
+  }
+
+
+  if (eventType === "organizationMembership.deleted") {
+    try {
+
+      const { organization, public_user_data } = evnt?.data;
+      console.log("removed", evnt?.data);
+
+
+      await removeUserFromCommunity(public_user_data.user_id, organization.id);
+
+      return NextResponse.json({ message: "Member removed" }, { status: 201 });
+    } catch (err) {
+      console.log(err);
+
+      return NextResponse.json(
+        { message: "Internal Server Error" },
+        { status: 500 }
+      );
+    }
+  }
+
+
+  if (eventType === "organization.updated") {
+    try {
+
+      const { id, logo_url, name, slug } = evnt?.data;
+      console.log("updated", evnt?.data);
+
+
+      await updateCommunityInfo(id, name, slug, logo_url);
+
+      return NextResponse.json({ message: "Member removed" }, { status: 201 });
+    } catch (err) {
+      console.log(err);
+
+      return NextResponse.json(
+        { message: "Internal Server Error" },
+        { status: 500 }
+      );
+    }
+  }
+
+
+  if (eventType === "organization.deleted") {
+    try {
+
+      const { id } = evnt?.data;
+      console.log("deleted", evnt?.data);
+
+
+      await deleteCommunity(id);
+
+      return NextResponse.json(
+        { message: "Organization deleted" },
+        { status: 201 }
+      );
+    } catch (err) {
+      console.log(err);
+
+      return NextResponse.json(
+        { message: "Internal Server Error" },
+        { status: 500 }
+      );
+    }
+  }
+};
--- next.config.js
+++ next.config.js
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    experimental: {
+      serverActions: true,
+      serverComponentsExternalPackages: ["mongoose"],
+    },
+    eslint: {
+      
+      ignoreDuringBuilds: true,
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "img.clerk.com",
+        },
+        {
+          protocol: "https",
+          hostname: "images.clerk.dev",
+        },
+        {
+          protocol: "https",
+          hostname: "uploadthing.com",
+        },
+        {
+          protocol: "https",
+          hostname: "placehold.co",
+        },
+      ],
+    },
+  };
+  
+  module.exports = nextConfig;
+  
