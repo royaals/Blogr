b65d24e696cd3eb2e0670c5b9e99d13faa1ad4a9
Royal <royal.21ai040@sode-edu.in>
2024-05-24 11:24:08 +0530

    update docker-compose.yml

--- app/(root)/notifications/page.tsx
+++ app/(root)/notifications/page.tsx
@@ -18,22 +18,26 @@ async function Page() {
     <>
       <h1 className='head-text'>Activity</h1>
 
-      <section className=' mt-10 flex flex-col gap-5'>
+      <section className='mt-10 flex flex-col gap-5'>
         {activity.length > 0 ? (
           <>
             {activity.map((activity) => (
               <Link key={activity._id} href={`/thread/${activity.parentId}`}>
-                <article className='activity-card  no-focus border border-dark-4'>
-                  <Image
-                    src={activity.author.image}
-                    alt='user_logo'
-                    width={20}
-                    height={20}
-                    className='rounded-full object-cover'
-                  />
+                <article className='activity-card no-focus border border-dark-4'>
+                  {activity.author?.image ? (
+                    <Image
+                      src={activity.author.image}
+                      alt='user_logo'
+                      width={20}
+                      height={20}
+                      className='rounded-full object-cover'
+                    />
+                  ) : (
+                    <div className='placeholder-image' style={{ width: 20, height: 20 }} />
+                  )}
                   <p className='!text-small-regular text-light-1'>
                     <span className='mr-1 text-primary-500'>
-                      {activity.author.name}
+                      {activity.author?.name || 'Unknown User'}
                     </span>{" "}
                     replied to your post
                   </p>
